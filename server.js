0dcfd45603dd879cb69ed596ac5da11689334c97
kjayaku1 <157340374+kjayaku1@users.noreply.github.com>
2024-10-06 21:26:37 -0400

    server.js

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
-// Endpoint that will forward requests to your AWS Lambda function
+
 app.get('/say', async (req, res) => {
   const keyword = req.query.keyword;  // Get the 'keyword' query param
 
   try {
-    // Use Axios to forward the request to your AWS Lambda function (kalai.js)
+    
     const response = await axios.get('https://st54st70a9.execute-api.us-east-2.amazonaws.com/default/say', {
-      params: { keyword }  // Forward the 'keyword' query param
+      params: { keyword }  
     });
 
-    // Send back the response from Lambda to the client
+   
     res.send(response.data);
   } catch (error) {
     console.error(error);
@@ -21,7 +21,7 @@ app.get('/say', async (req, res) => {
   }
 });
 
-// Start the local server
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
