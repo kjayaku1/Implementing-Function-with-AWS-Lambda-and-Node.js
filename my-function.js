904d877a309cce7bc55793f31d567d439a98d723
kjayaku1 <157340374+kjayaku1@users.noreply.github.com>
2024-10-06 21:28:26 -0400

    my-function.js

--- my-function.js
+++ my-function.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     const name = "Kalai Arasi Jayakumar"; 
-    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : "default";
+    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : "Hello";
 
     const responseMessage = `${name} says ${keyword}`;
 
